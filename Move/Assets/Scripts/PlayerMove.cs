aece468281ddfa1bab8e0a2d0df349f3fa9740b6
Naciye Öngün <naciyeongun@hotmail.com>
2021-10-09 19:22:58 +0300

    move

---
+++ Move/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    //unity playermove script te speed değerini vermeyi unutmayın.
+
+    private Rigidbody2D rigidbody;
+    public float speed;
+    private float moveInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+     void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rigidbody.velocity = new Vector2(moveInput * speed, rigidbody.velocity.y);
+    }
+}
